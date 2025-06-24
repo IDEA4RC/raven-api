e4572f8afc3bc0d97a9a9cb65593be366f976d37
Alejandro Alonso Lopez <aalonsopersonal@gmail.com>
2025-06-24 16:50:28 +0200

    changed schemas parameters

--- app/schemas/workspace_history.py
+++ app/schemas/workspace_history.py
@@ -16,14 +16,14 @@ class WorkspaceHistoryBase(BaseModel):
 
 class WorkspaceHistoryCreate(WorkspaceHistoryBase):
     """Schema para crear un historial de espacio de trabajo."""
-    user_id: int
+    creator_id: int
     workspace_id: int
 
 
 class WorkspaceHistory(WorkspaceHistoryBase):
     """Schema para leer un historial de espacio de trabajo."""
     id: int
-    user_id: int
+    creator_id: int
     workspace_id: int
     
     model_config = ConfigDict(from_attributes=True)
